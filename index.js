66bd14067fb3e786e1fba7b37ad00fa34c271b74
Your Name <you@example.com>
2022-04-29 17:50:00 +0600

    add ing heroku steps

--- index.js
+++ index.js
@@ -2,17 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //Middle Ware
 app.use(cors());
 app.use(express.json());
+require('dotenv').config()
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ldp2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const perfumeCollection = client.db('perfumeStores').collection('perfume');
+    
+        app.get('/perfume', async (req, res) =>{
+           const query = {};
+           const cursor = perfumeCollection.find(query);
+
+           const perfumes = await cursor.toArray();
+
+           res.send(perfumes);
+        })
+    
+    }
+    finally{}
+}
+
+run().catch(console.dir)
 
 
 
 app.get('/', (req, res) =>{
-    res.send('Hello from perfume stores')
+    res.send('Hello from perfume stores with heroku server')
 })
 
 app.listen(port, () =>{
