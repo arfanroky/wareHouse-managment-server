0de60418024855c4e6c2102f3975f7e21e8d8cf8
Your Name <you@example.com>
2022-05-08 17:29:41 +0600

    added corse policy

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 //Middle Ware
-app.use(cors());
 app.use(express.json());
 require('dotenv').config()
 
